08899c54597843e6e8a6a3df08a5afc96f4fa8be
Jaczibeth <jaczicruz@gmail.com>
2025-10-26 00:43:07 -0600

    Restructuracion de BarraPestañas

--- src/Componentes/BarraPestanas.js
+++ src/Componentes/BarraPestanas.js
@@ -11,46 +11,25 @@ export default function BarraPestanas({ pestanaActiva, alCambiarPestana }) {
           <TouchableOpacity
             key={pestana}
             style={[styles.pestana, activo && styles.pestanaActiva]}
-            onPress={() => alCambiarPestana(pestana)}
-          >
+            onPress={() => alCambiarPestana(pestana)} >
             <Text style={[styles.texto, activo && styles.textoActivo]}>
               {pestana
                 .split(" ")
                 .map(
                   (word) =>
-                    word.charAt(0).toUpperCase() +
-                    word.slice(1).toLowerCase()
+                    word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
                 )
                 .join(" ")}
             </Text>
           </TouchableOpacity>
         );
       })}
-    </View>
-  );
-}
+    </View> );}
 
 const styles = StyleSheet.create({
-  contenedor: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    marginVertical: 10,
-  },
-  pestana: {
-    paddingVertical: 8,
-    paddingHorizontal: 16,
-    borderBottomWidth: 2,
-    borderBottomColor: "transparent",
-  },
-  pestanaActiva: {
-    borderBottomColor: "#144784",
-  },
-  texto: {
-    fontSize: 16,
-    color: "#777",
-    fontWeight: "600",
-  },
-  textoActivo: {
-    color: "#144784",
-  },
+  contenedor: { flexDirection: "row", justifyContent: "space-around", marginVertical: 10,},
+  pestana: { paddingVertical: 8, paddingHorizontal: 16, borderBottomWidth: 2, borderBottomColor: "transparent",},
+  pestanaActiva: { borderBottomColor: "#144784",},
+  texto: { fontSize: 16,color: "#777", fontWeight: "600",},
+  textoActivo: { color: "#144784",},
 });
